366628b936f23c77dfd8b57b0b35f244a6da8a31
Jayant-Kumar-794 <jk4288@srmist.edu.in>
2026-05-05 23:45:44 +0530

    Coded UC6 to update the board with the given symbol

---
+++ TicTacToe_6.java
@@ -0,0 +1,16 @@
+/*
+*@author:Jayant Kumar
+*@version:1.0
+*/
+
+public class TicTacToe_6 {
+    static char[][] board = new char [3][3];
+    public static void main (String[] args ) {
+        placeMove(0,0,'X');
+        System.out.println(board[0][0]);
+    }
+
+    static void placeMove(int row, int column , char symbol){
+        board[row][column] = symbol;
+    }
+}
